be93eae087e5a3df6073a9a49613dd20b9a35283
Martin Mantelli <rmartinmantelli@gmail.com>
2025-04-20 18:40:26 -0300

    Le quito los 2 segundos de retardo al renderizar los productos en el archivo fetchData

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -4,7 +4,7 @@ export function fetchData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(products)
-    }, 2000)
+    }, )
   })
 }
 
@@ -12,6 +12,6 @@ export function fetchDataById(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(products.find(prod => prod.id === parseInt(id)))
-    }, 2000)
+    }, )
   })
 }
