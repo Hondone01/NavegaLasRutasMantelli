e6e4abe0d8ea57d9c89de24fbf29b29a6546e658
Martin Mantelli <rmartinmantelli@gmail.com>
2025-04-20 21:27:49 -0300

    se modifica apenas el componente contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const Contact = () => {
-  return <div>Contact</div>
+  return <div className='contacto'>
+    <h1>Contacto</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id inventore dolor aperiam nesciunt atque voluptatem vel, unde quidem sunt tempore doloremque corporis eaque deserunt architecto perferendis esse cum repudiandae non?</p>
+   </div>
 }
 
 export default Contact
